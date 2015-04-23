28b32ff64d5f1de96707f8ff2fffd7f1edf58037
Ivan <ivangomezedo@gmail.com>
2015-04-23 22:37:58 +0200

    file upload

--- src/main/resources/static/bower_components/js/homeController.js
+++ src/main/resources/static/bower_components/js/homeController.js
@@ -1,4 +1,4 @@
-var homeApp = angular.module('homeApp', ['angularUtils.directives.dirPagination']);
+var homeApp = angular.module('homeApp', ['angularUtils.directives.dirPagination','angularFileUpload']);
 homeApp.controller('homeController', function($scope, $http) {
 	$scope.iconHomePlus = "glyphicon glyphicon-plus";
 	$scope.viewConf = false;
