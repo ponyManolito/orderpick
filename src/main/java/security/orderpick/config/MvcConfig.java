48f5fd430dfabfb6b4ecac5e6bc8d76a0b9b2a8b
Ivan Gomez <ivan.gomez@fon.com>
2015-04-23 15:22:49 +0200

    Set language selector

--- src/main/java/security/orderpick/config/MvcConfig.java
+++ src/main/java/security/orderpick/config/MvcConfig.java
@@ -1,31 +1,51 @@
 package security.orderpick.config;
 
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
-    
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/vieworder").setViewName("forms/orders");
-        registry.addViewController("/viewusers").setViewName("forms/users");
-        registry.addViewController("/viewpaymentsdata").setViewName("forms/paymentsdata");
-        registry.addViewController("/viewproducts").setViewName("forms/products");
-        registry.addViewController("/viewtables").setViewName("forms/tables");
-        registry.addViewController("/viewturns").setViewName("forms/turns");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
-    }
-    
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-            registry.addResourceHandler("/static/**")
-                    .addResourceLocations("classpath:/static/");
-    }
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/vieworder").setViewName("forms/orders");
+		registry.addViewController("/viewusers").setViewName("forms/users");
+		registry.addViewController("/viewpaymentsdata").setViewName("forms/paymentsdata");
+		registry.addViewController("/viewproducts").setViewName("forms/products");
+		registry.addViewController("/viewtables").setViewName("forms/tables");
+		registry.addViewController("/viewturns").setViewName("forms/turns");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		Locale es = new Locale("es");
+		slr.setDefaultLocale(es);
+		return slr;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		LocaleChangeInterceptor locale = new LocaleChangeInterceptor();
+		locale.setParamName("language");
+		registry.addInterceptor(locale);
+	}
 
 }
