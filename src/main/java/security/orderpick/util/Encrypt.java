51c9cfec52552f3874afe9aca8c203449650a838
Ivan Gomez <ivan.gomez@fon.com>
2015-06-12 16:25:52 +0200

    Change ciper algorithm

--- src/main/java/security/orderpick/util/Encrypt.java
+++ src/main/java/security/orderpick/util/Encrypt.java
@@ -1,131 +1,89 @@
 package security.orderpick.util;
 
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.AlgorithmParameterSpec;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
 
 import javax.crypto.Cipher;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.PBEParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.commons.lang.ArrayUtils;
 import org.springframework.stereotype.Component;
 
 @Component(Encrypt.name)
-public class Encrypt implements PasswordEncoder {
+public class Encrypt {
 
 	public static final String name = "encrypt";
 
-	Cipher ecipher;
+	private final byte[] keyValue;
 
-	Cipher dcipher;
-
-	/**
-	 * Constructor used to create this object. Responsible for setting and initializing this
-	 * object's encrypter and decrypter Chipher instances given a Pass Phrase and algorithm.
-	 * 
-	 * @param passPhrase Pass Phrase used to initialize both the encrypter and decrypter instances.
-	 */
 	public Encrypt() {
 
-		// 8-bytes Salt
-		byte[] salt = { (byte) 0xA9, (byte) 0x9B, (byte) 0xC8, (byte) 0x32, (byte) 0x56, (byte) 0x34, (byte) 0xE3,
-				(byte) 0x03 };
-		String passPhrase = "OrderIGE83";
-		// Iteration count
-		int iterationCount = 19;
-
-		try {
-
-			KeySpec keySpec = new PBEKeySpec(passPhrase.toCharArray(), salt, iterationCount);
-			SecretKey key = SecretKeyFactory.getInstance("PBEWithMD5AndDES").generateSecret(keySpec);
-
-			ecipher = Cipher.getInstance(key.getAlgorithm());
-			dcipher = Cipher.getInstance(key.getAlgorithm());
-
-			// Prepare the parameters to the cipthers
-			AlgorithmParameterSpec paramSpec = new PBEParameterSpec(salt, iterationCount);
-
-			ecipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
-			dcipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
-
-		} catch (InvalidAlgorithmParameterException e) {
-			System.out.println("EXCEPTION: InvalidAlgorithmParameterException");
-		} catch (InvalidKeySpecException e) {
-			System.out.println("EXCEPTION: InvalidKeySpecException");
-		} catch (NoSuchPaddingException e) {
-			System.out.println("EXCEPTION: NoSuchPaddingException");
-		} catch (NoSuchAlgorithmException e) {
-			System.out.println("EXCEPTION: NoSuchAlgorithmException");
-		} catch (InvalidKeyException e) {
-			System.out.println("EXCEPTION: InvalidKeyException");
-		}
+		this.keyValue = "OrderIGE83".getBytes();
 	}
 
-	/**
-	 * Takes a single String as an argument and returns an Encrypted version of that String.
-	 * 
-	 * @param str String to be encrypted
-	 * @return <code>String</code> Encrypted version of the provided String
-	 * @throws Exception
-	 */
-	public String encrypt(String str) throws Exception {
+	public String encrypt(String plain) throws Exception {
+		if (plain == null) {
+			throw new IllegalArgumentException("String to encode cannot be null");
+		}
+		byte[] iv = new byte[16];
+		new Random().nextBytes(iv);
+
+		Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+		SecretKeySpec key = new SecretKeySpec(keyValue, "AES");
+		cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
+
+		byte[] padPlain = padString(plain);
+
+		byte[] resultInBytes = cipher.doFinal(padPlain);
+
+		resultInBytes = ArrayUtils.addAll(iv, resultInBytes);
+
+		return Hex.encodeHexString(resultInBytes);
+	}
+
+	public String decrypt(String encoded) throws Exception {
+		if (encoded == null) {
+			throw new IllegalArgumentException("String to decode cannot be null");
+		}
+		byte[] decodeHex = Hex.decodeHex(encoded.toCharArray());
+		byte[] cipherText = ArrayUtils.subarray(decodeHex, 16, decodeHex.length);
+		byte[] iv = ArrayUtils.subarray(decodeHex, 0, 16);
+
+		Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding", "SunJCE");
+		SecretKeySpec key = new SecretKeySpec(keyValue, "AES");
+
+		cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+		byte[] doFinal = cipher.doFinal(cipherText);
+
+		return new String(doFinal, "UTF-8").trim();
+	}
+
+	private byte[] padString(String source) throws UnsupportedEncodingException {
+		int blockSize = 16;
+		int padLength = blockSize - source.getBytes().length % blockSize;
+
+		byte[] bytes = new byte[padLength + source.getBytes().length];
+		byte[] original = source.getBytes("UTF-8");
+
+		for (int i = 0; i < original.length; i++) {
+			bytes[i] = original[i];
+		}
+		for (int i = original.length; i < bytes.length; i++) {
+			bytes[i] = 0;
+		}
+
+		return bytes;
+	}
+
+	public static void main(String[] args) {
+		Encrypt encrypt = new Encrypt();
 		try {
-			// Encode the string into bytes using utf-8
-			byte[] utf8 = str.getBytes("UTF8");
-
-			// Encrypt
-			byte[] enc = ecipher.doFinal(utf8);
-
-			// Encode bytes to base64 to get a string
-			return new sun.misc.BASE64Encoder().encode(enc);
-
+			System.out.println(encrypt.encrypt("Ivan2"));
 		} catch (Exception e) {
-			throw new Exception("Problem encrypting");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
-
-	/**
-	 * Takes a encrypted String as an argument, decrypts and returns the decrypted String.
-	 * 
-	 * @param str Encrypted String to be decrypted
-	 * @return <code>String</code> Decrypted version of the provided String
-	 * @throws Exception
-	 */
-	public String decrypt(String str) throws Exception {
-
-		try {
-
-			// Decode base64 to get bytes
-			byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(str);
-
-			// Decrypt
-			byte[] utf8 = dcipher.doFinal(dec);
-
-			// Decode using utf-8
-			return new String(utf8, "UTF8");
-
-		} catch (Exception e) {
-			throw new Exception("Problem encrypting");
-		}
-	}
-
-	@Override
-	public String encode(CharSequence rawPassword) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean matches(CharSequence rawPassword, String encodedPassword) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
