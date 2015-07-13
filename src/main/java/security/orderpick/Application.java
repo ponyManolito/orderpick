b5e82edfb5df322a336cdbd454f1c6bcf3fc462b
Ivan Gomez <ivan.gomez@fon.com>
2015-07-13 13:25:51 +0200

    Fix hazelcast

--- src/main/java/security/orderpick/Application.java
+++ src/main/java/security/orderpick/Application.java
@@ -3,12 +3,23 @@ package security.orderpick;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 
 @EnableAutoConfiguration
 @ComponentScan
 @SpringBootApplication
+@ImportResource({ "classpath:hazelcast-config.xml" })
 public class Application {
+
+	@Bean
+	public HazelcastInstance hazelcast() throws Exception {
+		return Hazelcast.newHazelcastInstance();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
